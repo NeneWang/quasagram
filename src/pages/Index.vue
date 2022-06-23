204b40506644c89e77ebb193950c48093149dee0
NelsonWang <wangnelson2@gmail.com>
2022-06-23 16:46:42 -0400

    vue | 14 | b

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,34 +1,38 @@
 <template>
   <q-page padding>
     <ul>
-      <task 
+      <task
         v-for="(task, index) in tasks"
+        :key="task.id"
         :task="task"
         :index="index"
-        >Something Insde the Slot for {{task.name}}</task>
+        >Something Insde the Slot for {{ task.name }}</task
+      >
     </ul>
   </q-page>
 </template>
 
 <script>
-import Task from 'components/Task.vue';
-
+import Task from "components/Task.vue";
 
 export default {
   data() {
     return {
       tasks: [
         {
+          id: 1,
           name: "Go to shop",
           dueDate: "2019/05/12",
           dueTime: "18:30",
         },
         {
+          id: 2,
           name: "Get Bananas",
           dueDate: "2019/05/13",
           dueTime: "14:00",
         },
         {
+          id: 3,
           name: "Get Apples",
           dueDate: "2019/05/14",
           dueTime: "16:00",
@@ -38,12 +42,12 @@ export default {
   },
   methods: {
     deleteTask(index) {
-      this.tasks.splice(index, 1)
+      this.tasks.splice(index, 1);
       // The 1 means that only 1 item will be deleted
-    }
+    },
   },
   components: {
-    'task' : require('components/Task.vue').default
-  }
+    task: require("components/Task.vue").default,
+  },
 };
 </script>
