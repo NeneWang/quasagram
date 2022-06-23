392e7556a7335ada4cde3d2153cc2d2b56234d22
NelsonWang <wangnelson2@gmail.com>
2022-06-23 00:38:11 -0400

    init

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <h5>{{message}}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      message: 'I love Vue.js'
+    }
+  }
+};
+</script>
+
+<style></style>
