3398d9004aa3f77cb242c8cb15a6f0ad4542aa44
NelsonWang <wangnelson2@gmail.com>
2022-06-23 16:02:54 -0400

    vue | 14 | b

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,94 +1,15 @@
 <template>
-  <q-page padding>
-    <button style="position: absolute; right: 10px" @click="counter++">
-      {{ counter }}
-    </button>
-    <input
-      v-autofocus
-      @keyup="handleKeyUp"
-      v-model="message"
-      type="text"
-      :style="errorStyle"
-      ref="messageInput"
-    />
-    <button @click="clearMessage">Clear</button>
-
-    <div>
-      {{message.length}}
-    </div>
-
-    <h5 v-if="message.length" class="border-grey">{{ message }}</h5>
-    <h6 v-else>No message entered 😢</h6>
-    <hr />
-    <p>Uppercased Message: {{ messageUppercase }}</p>
-    <p>Lowercase Message: {{ message | messageLowercase }}</p>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
   </q-page>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      message: "I love Vue.js",
-      counter: 0,
-    };
-  },
-  computed: {
-    messageUppercase() {
-      console.log("MessageUppercase was fired");
-      return this.message.toUpperCase();
-    },
-    errorStyle(){
-      if(this.message.length > 22){
-        return 'color: red;'
-      }
-    }
-  },
-  methods: {
-    clearMessage() {
-      this.message = "";
-    },
-    handleKeyUp(e) {
-      console.log(e);
-      if (e.keyCode == 27) {
-        this.clearMessage();
-      } else if (e.keyCode == 13) {
-        alert(this.message);
-      }
-    },
-  },
-  filters: {
-    messageLowercase(value) {
-      return value.toLowerCase();
-    },
-  },
-  directives: {
-    autofocus: {
-      inserted(el) {
-        console.log("directive fired");
-        el.focus();
-      },
-    },
-  },
-  mounted(){
-    console.log(this.$refs)
-    this.$refs.messageInput.className = 'bg-green'
-  }
-};
+  name: 'PageIndex'
+}
 </script>
-
-<style>
-.border-grey {
-  border: 1px solid grey;
-}
-
-input,
-button {
-  font-size: 23px;
-}
-
-.error {
-  color: red;
-  background: pink;
-}
-</style>
