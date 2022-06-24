571257e8bc248764ecc2053473abb5b821927067
NelsonWang <wangnelson2@gmail.com>
2022-06-23 20:42:04 -0400

    camera | page layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,19 +1,28 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-tabs>
-        <q-route-tab
-          to="/"
-          icon="mail"
-          label="Home"
-          name="home" />
-        <q-route-tab
-          to="/camera"
-          icon="alarm"
-          label="Camera"
-          name="camera" />
-      </q-tabs>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
     </q-header>
+
+    <q-footer>
+      <q-tabs>
+        <q-route-tab to="/" icon="mail" label="Home" name="home" />
+        <q-route-tab to="/camera" icon="alarm" label="Camera" name="camera" />
+      </q-tabs>
+    </q-footer>
 
     <q-page-container>
       <router-view />
