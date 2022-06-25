4ee5c4e50ed6d876df35d9de0c2719e3eb635caf
NelsonWang <wangnelson2@gmail.com>
2022-06-25 17:37:05 -0400

    s4 | 35

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,31 +1,55 @@
 <template>
-  <q-page class="constrain q-pa-md" >
-  
+  <q-page class="constrain q-pa-md">
     <q-card class="my-card" flat bordered>
       <q-item>
         <q-item-section avatar>
           <q-avatar>
-            <img src="https://en.gravatar.com/userimage/154408686/cc0f6634352ebaba1c30d7bd2101dbcf.jpg?size=200">
+            <img
+              src="https://en.gravatar.com/userimage/154408686/cc0f6634352ebaba1c30d7bd2101dbcf.jpg?size=200"
+            />
           </q-avatar>
         </q-item-section>
 
         <q-item-section>
-          <q-item-label class="text-bold" >Nelson__Wang</q-item-label>
-          <q-item-label caption>
-            New York, United States
-          </q-item-label>
+          <q-item-label class="text-bold">Nelson__Wang</q-item-label>
+          <q-item-label caption> New York, United States </q-item-label>
         </q-item-section>
       </q-item>
 
       <q-separator />
 
-    </q-card>
+      <q-img class="card-post" src="https://cdn.quasar.dev/img/parallax2.jpg" />
 
+      <q-card-section>
+        <div class="text-h6">Golden Gate Bridge</div>
+        <div class="text-GREY">June 10 9:04AM</div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 export default {
   name: "PageHome",
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: "Golden Gate Bridge",
+        },
+        {
+          id: 1,
+          caption: "Golden Gate Bridge",
+        },
+      ],
+    };
+  },
 };
 </script>
+
+<style lang="sass">
+.card-post
+    .q-img
+        min-height: 200px
+</style>
