bb692b276905dece83b3efee0b82cef976ddd9fc
NelsonWang <wangnelson2@gmail.com>
2022-06-25 15:49:44 -0400

    s4 | 35

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,37 @@
 <template>
   <q-page class="constrain q-pa-md" >
-    <h5>Home Page</h5>
+  
+    <q-card class="my-card" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://en.gravatar.com/userimage/154408686/cc0f6634352ebaba1c30d7bd2101dbcf.jpg?size=200">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Nelosn__Wang</q-item-label>
+          <q-item-label caption>
+            Subhead
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-card-section horizontal>
+        <q-card-section>
+            Lorem
+        </q-card-section>
+
+        <q-separator vertical />
+
+        <q-card-section class="col-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </q-card-section>
+      </q-card-section>
+    </q-card>
+
   </q-page>
 </template>
 
