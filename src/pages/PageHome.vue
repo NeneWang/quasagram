8f6c810647b8eab823cb62720cd20b8f5ed08fb4
NelsonWang <wangnelson2@gmail.com>
2022-06-25 17:46:03 -0400

    s4 | 35

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -37,10 +37,30 @@ export default {
         {
           id: 1,
           caption: "Golden Gate Bridge",
+          date: 1656193420878,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
         },
         {
-          id: 1,
+          id: 2,
           caption: "Golden Gate Bridge",
+          date: 1656193420878,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 3,
+          caption: "Golden Gate Bridge",
+          date: 1656193420878,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 4,
+          caption: "Golden Gate Bridge",
+          date: 1656193420878,
+          location: "San Francisco, United States",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
         },
       ],
     };
