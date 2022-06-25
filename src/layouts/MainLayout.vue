75edc9ead83ac427d8aec92c58e2f24c5945aad7
NelsonWang <wangnelson2@gmail.com>
2022-06-25 02:35:38 -0400

    quasar | s3

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,14 +4,17 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title class="text-center"> Quasar App </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
     <q-footer class="bg-white">
-      <q-tabs class="text-grey-10" active-color="primary" indicator-color="transparent" >
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
         <q-route-tab to="/" icon="mail" label="Home" name="home" />
         <q-route-tab to="/camera" icon="alarm" label="Camera" name="camera" />
       </q-tabs>
@@ -32,8 +35,11 @@ export default {
 };
 </script>
 
-<style lang="sass" >
-  .q-footer
-    .q-tab__icon
-      font-size: 30px
+<style lang="sass">
+.q-toolbar__title
+  text-align: center
+
+.q-footer
+  .q-tab__icon
+    font-size: 30px
 </style>
