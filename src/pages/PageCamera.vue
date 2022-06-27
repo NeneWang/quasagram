e0a099fc0041060c4f7e5e472d975a84481cc25f
NelsonWang <wangnelson2@gmail.com>
2022-06-27 19:09:57 -0400

    s5 | 45

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,8 +1,29 @@
 <template>
   <q-page class="flex flex-center">
     <div class="camera-frame q-pa-md">
-        <img class="full-width" src="https://data.whicdn.com/images/342829695/original.jpg" alt="">
+      <img
+        class="full-width"
+        src="https://data.whicdn.com/images/342829695/original.jpg"
+        alt=""
+      />
     </div>
+    <div class="text-center q-pa-md v">
+      <q-btn color="grey-10" icon="eva-camera" round size="lg" />
+    </div>
+
+    <div class="row justify-center q-ma-md">
+      <q-input v-model="text" class="col" label="Caption" dense />
+    </div>
+
+    <div class="row justify-center q-ma-md">
+      <q-input v-model="text" class="col" label="Location" dense>
+        <template v-slot:append>
+          <q-btn round dense flat icon="eva-navigation-2-outline" />
+        </template>
+      </q-input>
+    </div>
+
+    <div class="row justify-center q-ma-md"></div>
   </q-page>
 </template>
 
@@ -12,9 +33,8 @@ export default {
 };
 </script>
 
-
-<style lang="sass" > 
-    .camera-frame
-        border: 2px solid $grey-10
-        border-radius: 10px
+<style lang="sass">
+.camera-frame
+    border: 2px solid $grey-10
+    border-radius: 10px
 </style>
