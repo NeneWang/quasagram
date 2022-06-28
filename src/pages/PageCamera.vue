f5ea5709014f10e70acfb22b196dc2652d114068
NelsonWang <wangnelson2@gmail.com>
2022-06-27 20:38:57 -0400

    s4 | 50

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,9 +1,15 @@
 <template>
   <q-page class="flex flex-center q-pa-md">
     <div class="camera-frame q-pa-md">
-      <video class="full-width" autoplay ref="video" />
+      <video v-show="!imageCaptured" class="full-width" autoplay ref="video" />
 
-      <canvas ref="canvas" class="full-width" height="240"> </canvas>
+      <canvas
+        v-show="imageCaptured"
+        ref="canvas"
+        class="full-width"
+        height="240"
+      >
+      </canvas>
 
       <!-- <img
         class="full-width"
@@ -55,6 +61,7 @@ export default {
         photo: null,
         date: Date.now(),
       },
+      imageCaptured: false,
     };
   },
   methods: {
@@ -76,6 +83,31 @@ export default {
 
       let context = canvas.getContext("2d");
       context.drawImage(video, 0, 0, canvas.width, canvas.width);
+      this.imageCaptured = true;
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL());
+    },
+    dataURItoBlob() {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(",")[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], { type: mimeString });
+      return blob;
     },
   },
   mounted() {
