cd9a2623dbedbcb73254b5711f9426654249c913
NelsonWang <wangnelson2@gmail.com>
2022-06-29 21:03:47 -0400

    nodemon

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 4000
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('I love node so hard!'))
 
 app.listen(port, () => console.log(`Example app listining at httl://localhost: ${port}`))
 
