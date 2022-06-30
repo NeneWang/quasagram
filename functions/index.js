4318e67131ff8307ef934fe9114f9c78a10dd626
NelsonWang <wangnelson2@gmail.com>
2022-06-30 18:40:23 -0400

    function, backend | 1

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.posts = functions.https.onRequest((request, response) => {
+  let post = [
+    {
+        caption: 'Golden Gate Bridge',
+        location: 'San Francisco'
+    },
+    {
+        caption: 'London Eye',
+        location: 'London'
+    }    
+  ]
+
+  response.send(posts);
+});
