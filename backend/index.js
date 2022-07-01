39b1a623cc48af01e1c7bd68d26531480e3b3439
NelsonWang <wangnelson2@gmail.com>
2022-07-01 05:46:20 -0400

    service account keys

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,38 @@
 const express = require('express')
-const app = express()
-var admin = require("firebase-admin");
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
 const port = 4000
+const app = express()
 
 
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const serviceAccount = require('./serviceAccountKey.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyC7Onj_jWu8vl7ZBmjfuimcqtvUAVtRKm8",
-    authDomain: "quasagram-57a30.firebaseapp.com",
-    projectId: "quasagram-57a30",
-    storageBucket: "quasagram-57a30.appspot.com",
-    messagingSenderId: "300964608650",
-    appId: "1:300964608650:web:2cfba7fd97a06347a94b73"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC7Onj_jWu8vl7ZBmjfuimcqtvUAVtRKm8",
+//     authDomain: "quasagram-57a30.firebaseapp.com",
+//     projectId: "quasagram-57a30",
+//     storageBucket: "quasagram-57a30.appspot.com",
+//     messagingSenderId: "300964608650",
+//     appId: "1:300964608650:web:2cfba7fd97a06347a94b73"
+// };
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const db = firebaseApp.firestore()
+// // Initialize Firebase
+// const firebaseApp = initializeApp(firebaseConfig);
+// const db = getFirestore(firebaseApp);
+
 
 app.get('/', (req, res) => res.send('I love node so hard!'))
 
 app.get('/posts', (req, res) => {
     let posts = []
-
-    posts = db.collection('posts').get().then(snapshot => {
-        snapshot.forEach((doc) => {
-            console.log(doc.id, '=>', doc.data())
-            posts.push(doc.data())
-        })
-        res.send(posts)
-    })
 })
 
 
@@ -47,6 +46,6 @@ app.get('/posts', (req, res) => {
 
 
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || port)
 
 
