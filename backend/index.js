d219509102b6cf3d96c182e653ff3df4825eec5c
NelsonWang <wangnelson2@gmail.com>
2022-07-01 05:20:06 -0400

    index |

--- backend/index.js
+++ backend/index.js
@@ -10,32 +10,30 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC7Onj_jWu8vl7ZBmjfuimcqtvUAVtRKm8",
-  authDomain: "quasagram-57a30.firebaseapp.com",
-  projectId: "quasagram-57a30",
-  storageBucket: "quasagram-57a30.appspot.com",
-  messagingSenderId: "300964608650",
-  appId: "1:300964608650:web:2cfba7fd97a06347a94b73"
+    apiKey: "AIzaSyC7Onj_jWu8vl7ZBmjfuimcqtvUAVtRKm8",
+    authDomain: "quasagram-57a30.firebaseapp.com",
+    projectId: "quasagram-57a30",
+    storageBucket: "quasagram-57a30.appspot.com",
+    messagingSenderId: "300964608650",
+    appId: "1:300964608650:web:2cfba7fd97a06347a94b73"
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-
+const db = firebaseApp.firestore()
 
 app.get('/', (req, res) => res.send('I love node so hard!'))
 
 app.get('/posts', (req, res) => {
-    let posts = [
-        {
-            caption: 'Golden Gate Bridge',
-            location: 'San Francisco'
-        },
-        {
-            caption: 'London Eye',
-            location: 'London'
-        }
-    ]
-    res.send(posts)
+    let posts = []
+
+    posts = db.collection('posts').get().then(snapshot => {
+        snapshot.forEach((doc) => {
+            console.log(doc.id, '=>', doc.data())
+            posts.push(doc.data())
+        })
+        res.send(posts)
+    })
 })
 
 
