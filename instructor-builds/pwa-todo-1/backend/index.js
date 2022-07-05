6f9cbccb058cc63e72e54108ba75f14519ba2dc5
NelsonWang <wangnelson2@gmail.com>
2022-07-05 15:55:36 -0400

    package.json

--- instructor-builds/pwa-todo-1/backend/index.js
+++ instructor-builds/pwa-todo-1/backend/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
+let admin = require('firebase-admin')
+
 
 // Configuring Express
 const app = express()
 
+
+// Config - Firebase
+
+var serviceAccount = require("./quasagram-service-account.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+let db = admin.firestore()
 
 // Endpoints
 
