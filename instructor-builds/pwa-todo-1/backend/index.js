7470540d5c5f22c9f6de96e8f296a7dfa9735744
NelsonWang <wangnelson2@gmail.com>
2022-07-05 15:40:53 -0400

    index | express

--- instructor-builds/pwa-todo-1/backend/index.js
+++ instructor-builds/pwa-todo-1/backend/index.js
@@ -1,1 +1,20 @@
 const express = require('express')
+
+// Configuring Express
+const app = express()
+
+
+// Endpoints
+
+app.get('/', (request, response) => {
+    response.send('I love Node so hard!')
+})
+
+// Listen
+
+app.listen(5001)
+
+
+
+
+
